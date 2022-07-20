c583d2b8c8167545d93ca9fce07f0c263e66920b
rk66777 <r.kanuri@zensar.com>
2022-07-20 17:35:37 +0530

    commit

--- src/router.js
+++ src/router.js
@@ -19,6 +19,7 @@ const Router = () => (
     <App>
         <Header />
         <Routes>
+          <Route path="" element={<Navigate to="adobecapstoneassingment" />}></Route>
           <Route path="adobecapstoneassignment" element={<Home />}></Route>
           <Route path="/productlisting" element={<ProductListing />}></Route>
           <Route path="products/:id" element={<ProductDetail />} />
